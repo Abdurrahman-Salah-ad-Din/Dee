8d60950b3c5b17e33bd7b85e4004340c5026d42d
Abdurrahman <abdosalahas.01128@gmail.com>
2024-03-21 20:28:39 +0200

    add colors

---
+++ composeApp/src/desktopMain/kotlin/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val editorBackgroundColor = Color(0xFF1F2228)
+val numberColor = Color(0xFF717888)
+val dataTypeColor = Color(0xFFC586C0)
+val variableColor = Color(0xFF9CDCFE)
+val stringLiteralColor = Color(0xFFCE9178)
+val operatorColor = Color(0xFFD4D4D4)
+val numericColor = Color(0xFF569CD6)
+val delimiterColor = Color(0xFFD4D4D4)
+val outputColor = Color(0xFFC4CBDA)
+val errorColor = Color(0xFFC44D56)
